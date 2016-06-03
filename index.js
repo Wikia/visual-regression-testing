980ff4923fa76a567b251feee474c1b3a2eb30b0
Bogna Knychala <bogna.ka@gmail.com>
2016-06-03 17:49:05 +0200

    Make in npm-package friendly

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require('./lib/runner').run();
+module.exports = {
+	browser: require('./lib/browser'),
+	config: require('./lib/config'),
+	driver: require('./lib/driver'),
+	runner: require('./lib/runner'),
+	testcase: require('./lib/testcase'),
+	webdrivercss: require('./lib/testcase')
+};
